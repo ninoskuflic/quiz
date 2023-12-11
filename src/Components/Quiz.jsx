25e74d4f1acec544d332080fc3c3c88fdc019898
ninoskuflic <32514898+ninoskuflic@users.noreply.github.com>
2023-12-11 15:47:30 +0100

    Version 1.0.2

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -16,10 +16,10 @@ export default function Quiz() {
     const [currentQuestion, setCurrentQuestion] = useState(0)
     const [showScore, setShowScore] = useState(false)
     const [score, setScore] = useState(0)
-    const [selectedQuiz, setSelectedQuiz] = useState(false);
+    const [selectedQuiz, setSelectedQuiz] = useState(null);
 
     // Feedback State
-    const [feedback, setFeedback] = useState('');
+    const [feedback, setFeedback] = useState(null);
     const [feedbackVisibility, setFeedbackVisibility] = useState(false);
 
     // API State
@@ -55,7 +55,7 @@ export default function Quiz() {
 
     // Select Quiz
     function selectQuiz(choice) {
-        setSelectedQuiz(true);
+        setSelectedQuiz(choice);
         const fetchData = async () => {
             try {
                 const response = await fetch(`https://api.learn.skuflic.com/${choice}`);
@@ -70,10 +70,137 @@ export default function Quiz() {
         fetchData();
     }
 
+    let resultMessage;
+    switch (selectedQuiz) {
+        case 'the-simpsons':
+            switch (true) {
+                case score <= 5:
+                    resultMessage = "D'oh! Looks like you might need to spend some more time in Springfield. Don't worry; even Homer had to start somewhere!";
+                    break;
+                case score <= 10:
+                    resultMessage = "Not bad, but you're not quite a Springfield resident yet. Keep watching, and soon you'll be quoting Homer and singing with Lisa!";
+                    break;
+                case score <= 13:
+                    resultMessage = "Excellent! You're on your way to becoming a Springfield superstar. Just a little more knowledge, and you'll fit right in at Moe's Tavern.";
+                    break;
+                case score <= 15:
+                    resultMessage = "Cowabunga! You're a true citizen of Springfield! You know your Flanders from your Burns. Keep living the Simpsons dream!";
+                    break;
+                default:
+                    resultMessage = "Invalid score";
+                    break;
+            }
+            break;
+
+        case 'family-guy':
+            switch (true) {
+                case score <= 5:
+                    resultMessage = "Oh no! Looks like you need to spend more time in Quahog. Stewie would suggest time travel to give it another shot.";
+                    break;
+                case score <= 10:
+                    resultMessage = "Not bad, but you're not quite a Griffin yet. Keep watching, and soon you'll be enjoying life in Quahog with Peter and the gang!";
+                    break;
+                case score <= 13:
+                    resultMessage = "Well done! You're on your way to becoming a Quahog expert. Just a bit more Peter wisdom, and you'll fit right in at The Drunken Clam.";
+                    break;
+                case score <= 15:
+                    resultMessage = "Freakin' sweet! You're a true Family Guy aficionado. You and Brian would have a great time discussing philosophy and literature.";
+                    break;
+                default:
+                    resultMessage = "Invalid score";
+                    break;
+            }
+            break;
+
+        case 'futurama':
+            switch (true) {
+                case score <= 5:
+                    resultMessage = "Good news, everyone! It seems you need a bit more time in the 31st century. Keep watching, and you'll be a Planet Express crew member in no time!";
+                    break;
+                case score <= 10:
+                    resultMessage = "Not bad, but you're not quite a future expert yet. Keep exploring the cosmos with Fry, Bender, and Leela!";
+                    break;
+                case score <= 13:
+                    resultMessage = "Great job! You're on your way to becoming a true Planet Express employee. Just a little more knowledge, and you'll be navigating the space-time continuum.";
+                    break;
+                case score <= 15:
+                    resultMessage = "Congratulations, space cadet! You're a true Futurama fan. Bender would be proud to call you his friend!";
+                    break;
+                default:
+                    resultMessage = "Invalid score";
+                    break;
+            }
+            break;
+
+        case 'rick-and-morty':
+            switch (true) {
+                case score <= 5:
+                    resultMessage = "Wubba lubba dub-dub! It looks like you need more adventures with Rick and Morty. Get schwifty and try again!";
+                    break;
+                case score <= 10:
+                    resultMessage = "Not bad, but you're not quite a dimension-hopping genius yet. Keep watching, and you'll be understanding Rick's burps in no time!";
+                    break;
+                case score <= 13:
+                    resultMessage = "Great job! You're on your way to becoming a member of the Council of Ricks. Just a bit more interdimensional knowledge, and you'll be ready for anything.";
+                    break;
+                case score <= 15:
+                    resultMessage = "Wubba lubba dub-dub! You're a true Rick and Morty genius! Time to celebrate with some Szechuan sauce!";
+                    break;
+                default:
+                    resultMessage = "Invalid score";
+                    break;
+            }
+            break;
+
+        case 'harry-potter':
+            switch (true) {
+                case score <= 5:
+                    resultMessage = "Mischief not managed! Looks like you need a few more spells in your repertoire. Keep studying at Hogwarts, and you'll be a wizard in no time!";
+                    break;
+                case score <= 10:
+                    resultMessage = "Not bad, but you're not quite a wizard yet. Keep practicing your charms and potions, and you'll be ready for Quidditch in no time!";
+                    break;
+                case score <= 13:
+                    resultMessage = "Well done! You're on your way to becoming a true wizard or witch. Just a bit more magical knowledge, and you'll ace your O.W.L.s.";
+                    break;
+                case score <= 15:
+                    resultMessage = "Bravo, wizarding prodigy! You're a true expert in the magical world of Harry Potter. Grab your wand and continue your magical journey!";
+                    break;
+                default:
+                    resultMessage = "Invalid score";
+                    break;
+            }
+            break;
+
+        case 'game-of-thrones':
+            switch (true) {
+                case score <= 5:
+                    resultMessage = "Winter is coming, and so is more study time! Looks like you need to brush up on your Westeros knowledge. Keep watching, and you'll be a lord or lady in no time!";
+                    break;
+                case score <= 10:
+                    resultMessage = "Not bad, but you're not quite a ruler of the Seven Kingdoms yet. Keep watching, and soon you'll be playing the Game of Thrones like a pro!";
+                    break;
+                case score <= 13:
+                    resultMessage = "Well done! You're on your way to becoming a true Westeros strategist. Just a bit more political intrigue, and you'll be ready for the Iron Throne.";
+                    break;
+                case score <= 15:
+                    resultMessage = "Hail to the ruler of Westeros! You're a true Game of Thrones maestro. Prepare your dragons, because you're destined for greatness!";
+                    break;
+                default:
+                    resultMessage = "Invalid score";
+                    break;
+            }
+            break;
+
+        default:
+            resultMessage = "Invalid category";
+            break;
+    }
+
+
     return (
         <>  {!selectedQuiz && <div className='info'>
-            <h1>Select Your Quiz</h1>
-            <p>Ready to test your knowledge?</p>
+            <p>Select your quiz to get started</p>
             <div className='general-section quiz-selection'>
                 <button onClick={() => selectQuiz('the-simpsons')}>
                     <img src={TheSimpsons} alt='The Simpsons' className='logo' />
@@ -107,7 +234,7 @@ export default function Quiz() {
                     <div className='score-section'>
                         <img src={Sparkels} alt='Sparkles' />
                         You've scored {score} out of {questions?.length}
-                        <p>We hope you've had fun!</p>
+                        <p>{resultMessage}</p>
                         <button onClick={() => window.location = ''} className='general-section'>Play Again</button>
                     </div>
                 ) :
